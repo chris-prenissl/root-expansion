2ef104a15798ce3a483bd38a1b1d8a2e8a860656
Christoh Prenissl <chris@celrage.com>
2023-02-05 15:38:41 +0100

    added DayNight Cycle

---
+++ Scripts/DayNight.gd
@@ -0,0 +1,31 @@
+extends Control
+
+@export var time_speed = 0.04
+@export var foreground: TextureRect
+@export var button: Button
+
+@onready var bar_length = foreground.size.x 
+
+var day_value: float
+
+func _ready():
+	day_value = 1
+	button.button_down.connect(reset_day_value)
+	
+func _process(delta):
+	print(day_value)
+	day_value -= time_speed*delta
+	
+	if day_value <= 0:
+		day_value = 0
+	
+	set_value()
+
+func set_value():
+	foreground.size = Vector2(day_value*bar_length, foreground.size.y)
+	
+func reset_day_value():
+	day_value = 1.0
+	
+func get_day_time_value() -> float:
+	return day_value
